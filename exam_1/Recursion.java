7770f41535e04b8ac90d34688f9f4e8b99227c45
Joshua Guimbaolibot <119920263+jumbo-hotdog@users.noreply.github.com>
2023-01-31 23:46:36 +0800

    Add files via upload

---
+++ exam_1/Recursion.java
@@ -0,0 +1,60 @@
+public class Recursion {
+
+    public static void main(String[] args) {
+
+        // Remove letters (30 pts)
+
+        // Instructions (Important!)
+
+        // Without using arrays or loops or regular expressions, apply recursion to remove specific letters in
+        // your full name.
+
+        // 1. If the last letter of your surname is a vowel, remove all vowels in
+        // your full name.
+
+        // 2. If the last letter of your surname is a consonant, remove all
+        // consonants in your full name.
+
+        // 3. Use only lowercase letters
+
+        // 4. Each letter should be removed one by one.
+
+        // Example:
+        // Full name is "elizer ponio jr".
+        // The output should be "lzr pn jr".
+
+        // Put your complete name in the "fullName" variable.
+        // Example:
+        // String fullName = "elizer ponio jr";
+
+        String fullName = "joshua guimbaolibot";
+
+        removeLetter(fullName);
+
+    }
+
+    static String removeLetter(String str) {
+        StringBuilder newStr = new StringBuilder(str);
+        System.out.println(newStr);
+        newStr.deleteCharAt(0);
+        System.out.println(newStr);
+        newStr.deleteCharAt(1);
+        System.out.println(newStr);
+        newStr.deleteCharAt(1);
+        System.out.println(newStr);
+        newStr.deleteCharAt(4);
+        System.out.println(newStr);
+        newStr.deleteCharAt(6);
+        System.out.println(newStr);
+        newStr.deleteCharAt(6);
+        System.out.println(newStr);
+        newStr.deleteCharAt(8);
+        System.out.println(newStr);
+        newStr.deleteCharAt(9);
+        System.out.println(newStr);
+        newStr.deleteCharAt(10);
+        System.out.println(newStr);
+
+        return str;
+        }
+    }
